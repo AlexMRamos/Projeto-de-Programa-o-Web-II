60572c2aa362b76cd13aaf71cffecc3f35877b26
Alex Monteiro Ramos <Alex.M.Ramos@hotmail.com>
2023-02-11 20:51:52 -0300

    criação das entidades

---
+++ src/main/java/ramos/monteiro/alex/bookstore/model/entity/Categoria.java
@@ -0,0 +1,25 @@
+package ramos.monteiro.alex.bookstore.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "categoria")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Categoria {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "nome", nullable = false, unique = true)
+    private String nome;
+}
---
+++ src/main/java/ramos/monteiro/alex/bookstore/model/entity/Livro.java
@@ -0,0 +1,37 @@
+package ramos.monteiro.alex.bookstore.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "livro")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Livro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "isbn", nullable = false, unique = true)
+    private String isbn;
+    @JoinColumn(name = "id_categoria", nullable = false)
+    @ManyToOne
+    private Categoria categoria;
+    @JoinColumn(name = "id_editora", nullable = false)
+    @ManyToOne
+    private Editora editora;
+    @Column(name = "nome", nullable = false, unique = true)
+    private String nome;
+}
